73464347868a9c542e527b4055d458e1809198e1
itsend <xhan87@gmail.com>
2022-03-07 10:15:06 +0800

    更新文件 todo.js

--- todo.js
+++ todo.js
@@ -6,13 +6,26 @@
 
 var body = $response.body;
 var obj = JSON.parse(body);
-
+/*
 obj['account']["promotions"] = [{
 				"code": "3DayUnlock_4_4_6",
 				"expireMs": 165156874400,
 				"status": 1
 			}]
 obj['isTrialActive'] = true
+*/
+obj['account']["subscriptionExpireDate"] = 1697001115690 ;
+obj['account']['isTrialActive'] = true ;
+obj['account']['transactions'] = [{
+				"original_purchase_date_ms": 1642669544000,
+				"customDescription": "Trial",
+				"duration": 3
+			}, {
+				"original_purchase_date_ms": 1646376315690,
+				"customDescription": "庆祝一千万下载",
+				"duration": 7
+			}]
+
 
 body = JSON.stringify(obj);
 console.log("Think2 body rewrited.");
